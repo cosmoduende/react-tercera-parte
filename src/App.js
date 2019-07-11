e75f3c5f80bcaffd8bcd7f5246d0e735550b9f73
Saúl Buentello <saulxfusion@gmail.com>
2019-07-10 20:55:39 -0500

    Primer commit del curso react desde cero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import "./styles/styles.scss";
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+<>
+    <div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt="banner" src="https://images.pexels.com/photos/597909/pexels-photo-597909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Curso de React</p>
+        <p> Probando ando</p>
+        <a href="http://www.kmmx.mx" className="button">Hazme clic</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+<div className="ed-grid m-grid-3">
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    <Curso/>
+</div>
+</>
+    )
 
 export default App;
---
+++ src/Curso.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const Curso = () => (
+    <>
+    <article className="card">
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+        <img src="https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="poster"/>
+    </div>
+    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+        <h3 className="t5 s-mb-2 s-center">
+        Programación orientada a objetos con Go
+        </h3>
+        <div className="s-mb-2 s-main-center">
+        <div className="card__teacher s-cross-center">
+            <div className="card__avatar s-mr-1">
+            <div className="circle img-container">
+                <img src="https://images.wallpaperscraft.com/image/road_marking_bridge_123398_300x168.jpg" alt="profesor"/>
+            </div>
+            </div>
+            <span className="small">Alexys Lozada</span>
+        </div>
+        </div>
+        <div className="s-main-center">
+        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+        </div>
+    </div>
+    </article>
+
+    <article className="card">
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+        <img src="https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="poster"/>
+    </div>
+    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+        <h3 className="t5 s-mb-2 s-center">
+        Programación orientada a objetos con Go
+        </h3>
+        <div className="s-mb-2 s-main-center">
+        <div className="card__teacher s-cross-center">
+            <div className="card__avatar s-mr-1">
+            <div className="circle img-container">
+                <img src="https://images.wallpaperscraft.com/image/road_marking_bridge_123398_300x168.jpg" alt="profesor"/>
+            </div>
+            </div>
+            <span className="small">Alexys Lozada</span>
+        </div>
+        </div>
+        <div className="s-main-center">
+        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+        </div>
+    </div>
+    </article>
+
+    <article className="card">
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+        <img src="https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="poster"/>
+    </div>
+    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+        <h3 className="t5 s-mb-2 s-center">
+        Programación orientada a objetos con Go
+        </h3>
+        <div className="s-mb-2 s-main-center">
+        <div className="card__teacher s-cross-center">
+            <div className="card__avatar s-mr-1">
+            <div className="circle img-container">
+                <img src="https://images.wallpaperscraft.com/image/road_marking_bridge_123398_300x168.jpg" alt="profesor"/>
+            </div>
+            </div>
+            <span className="small">Alexys Lozada</span>
+        </div>
+        </div>
+        <div className="s-main-center">
+        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+        </div>
+    </div>
+    </article>
+    </>
+)
+
+export default Curso;
