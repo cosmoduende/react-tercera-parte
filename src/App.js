6dbd0e33af1853d5fff4837d3d378d8db31d6242
Saúl Buentello <saulxfusion@gmail.com>
2019-07-13 12:30:12 -0500

    codigo realizado en avance de curso de react desde cero

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import React, {Fragment} from 'react';
 import "./styles/styles.scss";
 import Curso from './Curso';
 
+const cursos = [
+  {
+      "title": "React desde cero",
+      "image":"https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png",
+      "price": 30,
+      "profesor": "Saú Buentello"
+  },
+  {
+      "title": "Drupal desde cero",
+      "image":"https://www.drupal.org/sites/all/themes/bluecheese/images/og.jpg",
+      "price": 40,
+      "profesor": "Alexis Lozada"
+  }, 
+  {
+      "title": "Go desde cero",
+      "image":"https://pbs.twimg.com/profile_images/554798224154701824/mWd3laxO.png",
+      "price": 50,
+      "profesor": "Alex Market"
+  }, 
+  {
+      "title": "HTML desde cero",
+      "image":"https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/que-es-html.jpg",
+      "price": 10,
+      "profesor": "Mario Guerrero"
+  },  
+]
+
 const App = () => (
 <>
     <div className="main-banner img-container l-section" id="main-banner">
@@ -18,13 +45,26 @@ const App = () => (
 </div>
 
 <div className="ed-grid m-grid-3">
-    <Curso/>
-    <Curso/>
-    <Curso/>
-    <Curso/>
-    <Curso/>
-    <Curso/>
-    <Curso/>
+    <Curso 
+      title="React desde cero"
+      image="https://cdn-images-1.medium.com/max/1600/1*y6C4nSvy2Woe0m7bWEn4BA.png"
+      price = "20usd"
+      profesor = "Saúl Buentello"
+      />
+    
+    {
+      cursos.map( c => <Curso title={c.title} image={c.image} price={c.price} profesor={c.profesor}/> )
+    }
+
+    <Curso 
+      title="React desde cero"
+      image="https://cdn-images-1.medium.com/max/1600/1*y6C4nSvy2Woe0m7bWEn4BA.png"
+      price = "20usd"
+      profesor = "Saúl Buentello"
+      />
+    <Curso
+      
+    />
 </div>
 </>
     )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,79 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Curso = () => (
-    <>
+const mayorDeEdad = edad => edad > 18;
+
+const persona = {"nombre":"Saúl", "apellido":"Buentello", "edad":19};
+
+const curso ={"title":"React desde cero", "image":"https://cdn-images-1.medium.com/max/1600/1*y6C4nSvy2Woe0m7bWEn4BA.png", "price":"50usd"}
+
+const Curso = ({image, title, price, profesor}) => (
     <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="poster"/>
+        <img src={image} alt={title}/>
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-        </h3>
-        <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-            <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-                <img src="https://images.wallpaperscraft.com/image/road_marking_bridge_123398_300x168.jpg" alt="profesor"/>
-            </div>
-            </div>
-            <span className="small">Alexys Lozada</span>
-        </div>
+        <h3 className="center">{title}</h3>
+        <div className="s-main-center">
+            {`Prof.: ${profesor}`}
         </div>
         <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="#">{`$${price} USD`}</a>
         </div>
     </div>
     </article>
-
-    <article className="card">
-    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="poster"/>
-    </div>
-    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-        </h3>
-        <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-            <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-                <img src="https://images.wallpaperscraft.com/image/road_marking_bridge_123398_300x168.jpg" alt="profesor"/>
-            </div>
-            </div>
-            <span className="small">Alexys Lozada</span>
-        </div>
-        </div>
-        <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
-        </div>
-    </div>
-    </article>
-
-    <article className="card">
-    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="poster"/>
-    </div>
-    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-        </h3>
-        <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-            <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-                <img src="https://images.wallpaperscraft.com/image/road_marking_bridge_123398_300x168.jpg" alt="profesor"/>
-            </div>
-            </div>
-            <span className="small">Alexys Lozada</span>
-        </div>
-        </div>
-        <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
-        </div>
-    </div>
-    </article>
-    </>
 )
+
+Curso.propTypes ={
+    tile:PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    profeor: PropTypes.string
+}
+
+Curso.defaultProps ={
+    title: "No se encontró título",
+    image: "https://carolinadojo.com/wp-content/uploads/2017/04/default-image.jpg",
+    price: "--",
+    profesor: ""
+}
 
 export default Curso;
