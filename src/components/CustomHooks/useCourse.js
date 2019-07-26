1446d2f9d1ff3877ddfc2758cb429be580e0a1e6
Saúl Buentello <saulxfusion@gmail.com>
2019-07-26 17:39:22 -0500

    proyecto final segundo curso react con custom hooks

---
+++ src/components/CustomHooks/useCourse.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
+
+const useCourse = id => {
+
+    const [course, setCourse] = useState({})
+
+    useEffect(() =>{
+        axios.get(`http://my-json-server.typicode.com/cosmoduende/json-db/cursos/${id}`)
+        .then(resp => setCourse(resp.data))
+    }, [])
+
+    return course
+
+}
+
+export default useCourse;
--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,32 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import useCourse from "../CustomHooks/useCourse";
 
 const Course = ({match}) => {
 
-    const [state, setState] = useState({
-            id: 1,
-            title: "React desde cero uno",
-            image:"https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png",
-            price: 30,
-            profesor: "Saú Buentello"
-    })
+    const [comment, setComment] = useState("Sin comentarios")
+    const course = useCourse(match.params.id)
 
-const changeTitle = (text) => {
-    setState({
-        ...state,
-        title: text
-    })
-}
+    const myComment = e => {
+        setComment(e.target.value)
+    }
 
     return(
         <div className="ed-grid m-grid-3">
             {
-            state ? (
-                <>
-                 <h1 className="m-cols-3">{state.title}</h1>
-                 <img className="m-cols-1" src={state.image} alt={state.titulo}/>
-                 <p className="m-cols-2">Profesor: {state.profesor}</p>
-                 <button onClick={changeTitle.bind(this, "Go desde cero")}>Cambiar título</button>
-                </>
+            course ? (
+                <div className="ed-grid">
+                 <div className="l-blok">
+                 <h1 className="m-cols-3">{course.title}</h1>
+                 <img className="m-cols-1" src={course.image} alt={course.titulo}/>
+                 <p className="m-cols-2">Profesor: {course.professor}</p>
+                 </div>
+                 <div className="ed-grid">
+                     <h2>Escribe tu comentario</h2>
+                     <input type="text" placeholder="escribe" onChange={myComment.bind(this)}/>
+                     <p>{comment}</p>
+                 </div>
+                </div>
             ) :
             <h1>El curso no existe</h1>
             }
